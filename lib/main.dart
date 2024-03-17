152a7097f058fa4f50583f961c23ac8031365d56
RanaHisham18 <hishamrana00@gmail.com>
2024-03-18 00:22:06 +0200

    design adjusted

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -34,113 +35,160 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightBlue,
         title: Text("Points Calculator"),
       ),
-      body:Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text("Team A", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),),
-              ),
-              Text("0", style: TextStyle(fontSize: 64.0),),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 140.0,
-                  color: Colors.lightBlue,
-                  child: MaterialButton(onPressed: (){}, child: const Text("Add 1 point",
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Team A",
+                        style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const Text(
+                      "0",
+                      style: TextStyle(fontSize: 64.0),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 140.0,
+                        color: Colors.lightBlue,
+                        child: MaterialButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Add 1 point",
+                            style: TextStyle(fontSize: 20.0, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 140.0,
+                        color: Colors.lightBlue,
+                        child: MaterialButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Add 2 point",
+                            style: TextStyle(fontSize: 20.0, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 140.0,
+                        color: Colors.lightBlue,
+                        child: MaterialButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Add 4 point",
+                            style: TextStyle(fontSize: 20.0, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 140.0,
-                  color: Colors.lightBlue,
-                  child: MaterialButton(onPressed: (){}, child: const Text("Add 2 point",
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),),),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 64, 0, 0),
+                  child: SizedBox(
+                    height: 220,
+                    child: VerticalDivider(
+                      width: 20,
+                      thickness: 2,
+                      color: Colors.grey,
+                    ),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 140.0,
-                  color: Colors.lightBlue,
-                  child: MaterialButton(onPressed: (){}, child: const Text("Add 4 point",
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),),),
+                Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        "Team B",
+                        style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const Text(
+                      "0",
+                      style: TextStyle(fontSize: 64.0),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 140.0,
+                        color: Colors.lightBlue,
+                        child: MaterialButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Add 1 point",
+                            style: TextStyle(fontSize: 20.0, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 140.0,
+                        color: Colors.lightBlue,
+                        child: MaterialButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Add 2 point",
+                            style: TextStyle(fontSize: 20.0, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        width: 140.0,
+                        color: Colors.lightBlue,
+                        child: MaterialButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Add 4 point",
+                            style: TextStyle(fontSize: 20.0, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 64, 0, 264),
-            child: const VerticalDivider(
-              width: 20,
-              thickness: 2,
-              color: Colors.grey,
+              ],
             ),
-          ),
-          Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text("Team B", style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),),
-              ),
-              Text("0", style: TextStyle(fontSize: 64.0),),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 140.0,
-                  color: Colors.lightBlue,
-                  child: MaterialButton(onPressed: (){}, child: const Text("Add 1 point",
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),),),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 140.0,
-                  color: Colors.lightBlue,
-                  child: MaterialButton(onPressed: (){}, child: const Text("Add 2 point",
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),),),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 140.0,
-                  color: Colors.lightBlue,
-                  child: MaterialButton(onPressed: (){}, child: const Text("Add 4 point",
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),),),
-                ),
-              ),
-            ],
-          )
-        ],
-      ) // This trailing comma makes auto-formatting nicer for build methods.
+            MaterialButton(onPressed: (){},
+              child: const Text(
+                "Reset",
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+              ),)
+          ],
+
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
